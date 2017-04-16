46fe2defeda3dc97ac793b74f5d753089025111b
taoalpha <iamzhoutao92@gmail.com>
2017-04-16 13:54:06 -0700

    switch to async/await

--- src/models/user.js
+++ src/models/user.js
@@ -1,17 +1,9 @@
 const _ = require("lodash");
 
-function sequencePromise(data, eachFn) {
-  let result = Promise.resolve();
-  data.forEach(v => {
-    result = result.then(() => eachFn(v));
-  });
-  return result;
-}
-
 // Get a particular comment
 exports.update = function(name, data) {
   const userC = this.db.collection('user');
-  return userC.update({name}, { $push: { remembered: {$each: data.remembered || []} } });
+  return userC.update({name}, { $addToSet: { remembered: {$each: data.remembered || []} } });
 };
 
 
--- src/models/word.js
+++ src/models/word.js
@@ -1,65 +1,51 @@
 const books = require("../books"),
       _ = require("lodash");
 
-function sequencePromise(data, eachFn) {
-  let result = Promise.resolve();
-  data.forEach(v => {
-    result = result.then(() => eachFn(v));
-  });
-  return result;
-}
-
 // Get a particular comment
-exports.get = function(word) {
+exports.get = async function(word) {
   const wordC = this.db.collection('word');
   const volumeC = this.db.collection('volume');
 
-  return wordC.findOne({word}).then(doc => {
-    if (!doc) throw new Error("Not Found");
-    doc.sentences = [];
-    return sequencePromise(doc.examples, (vid) => {
-      return volumeC.findOne({vid: vid + ""}, {fields: {sentences: 1, meta: 1}}).then(vol => {
-        if (vol.sentences[doc.word]) doc.sentences = doc.sentences.concat(vol.sentences[doc.word].map(s => ({s: s, source: vol.meta.corpus.name})));
-        delete doc.examples;
-        return doc;
-      })
-    });
-  });
+  const doc = await wordC.findOne({word});
+  doc.sentences = [];
+  for (let vid of doc.examples) {
+    const vol = await volumeC.findOne({vid: vid + ""}, {fields: {sentences: 1, meta: 1}});
+    if (vol.sentences[doc.word]) {
+      doc.sentences = doc.sentences.concat(vol.sentences[doc.word].map(s => ({s: s, source: vol.meta.corpus.name})));
+    }
+  }
+  delete doc.examples;
+  return doc;
 };
 
-
-exports.getBatch = function(options) {
+exports.getBatch = async function(options) {
   options = options || {};
   options.batch = +options.batch > 0 ? +options.batch : 10;  // batch should be a valid number and fall back to 50
-  return books("gre").then(words => {
-    const userC = this.db.collection('user');
-    const wordC = this.db.collection('word');
-    const volumeC = this.db.collection('volume');
-    let curBatch = [];
 
-    // TODO: Need to be improved...
-    return userC.findOne({name: "xiaobao"}).then(userDoc => {
-      // get words for this batch
-      curBatch = _.sampleSize(_.difference(words, userDoc.vocabulary, userDoc.remembered), options.batch);
-      return curBatch;
-    }).then(words => {
-      return wordC.find({word: {$in: words}}).toArray().then(docs => {
-        if (docs.length === 0) throw new Error("Not Found");
-        return sequencePromise(docs, (doc) => {
-          doc.sentences = [];
-          return sequencePromise(doc.examples, (vid) => {
-            return volumeC.findOne({vid: vid + ""}, {fields: {sentences: 1, meta: 1}}).then(vol => {
-              if (vol.sentences[doc.word]) doc.sentences = doc.sentences.concat(vol.sentences[doc.word].map(s => ({s: s, source: vol.meta.corpus.name})));
-              delete doc.examples;
-              return docs;
-            });
-          });
-        });
-      });
-    }).then(docs => {
-      userC.update({name: "xiaobao"}, { $push: { vocabulary: { $each: curBatch } } });
-      return docs;
-    })
-    .catch(console.log)
-  })
+  // flow is:
+  //  - fetch current book and vocabulary of the user
+  //  - fetch book
+  //  - fetch this batch
+  const userC = this.db.collection('user');
+  const wordC = this.db.collection('word');
+  const volumeC = this.db.collection('volume');
+
+  const userDoc = await userC.findOne({name: "xiaobao"});
+  const totalWords = await books(userDoc.book);
+  const fetchWords = _.sampleSize(_.difference(totalWords, _.difference(userDoc.vocabulary, userDoc.remembered)), options.batch)
+
+  const wordDocs = await wordC.find({word: {$in: fetchWords}}).toArray();
+  for (let doc of wordDocs) {
+    doc.sentences = [];
+    for (let vid of doc.examples) {
+      const vol = await volumeC.findOne({vid: vid + ""}, {fields: {sentences: 1, meta: 1}});
+      if (vol.sentences[doc.word]) {
+        doc.sentences = doc.sentences.concat(vol.sentences[doc.word].map(s => ({s: s, source: vol.meta.corpus.name})));
+      }
+    }
+    delete doc.examples;
+  }
+
+  userC.update({name: "xiaobao"}, { $push: { vocabulary: { $each: fetchWords } } });
+  return wordDocs;
 };
